d56c203014c611bd99697e8bfe5d3fc1488a4bed
Jimmy Gonzalez <jimmygonzalez87@gmail.com>
2018-12-19 14:06:04 -0600

    started landing page

--- client/components/main_pages/Home.js
+++ client/components/main_pages/Home.js
@@ -4,7 +4,21 @@ class Home extends Component {
   render(){
     return (
       <div id='home' className=''>
-        Home
+        <div className='background-div' >
+        </div>
+        <div className='landing'>
+        </div>
+
+        <div className='title-section row'>
+          {/* <div className='col-2'>
+            <i className='material-icons'>home</i>
+          </div> */}
+          <div className='col-10'>
+            <h1>MyProperties.me</h1>
+            <h3>MyProperties.me is a property management app for landlords in a digital world.</h3>
+          </div>
+
+        </div>
       </div>
     )
   }
