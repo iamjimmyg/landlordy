62140cc5ceb1896e9b05810f56a5ab53282edc3e
Jimmy Gonzalez <jimmygonzalez87@gmail.com>
2019-01-03 09:37:40 -0600

    home page content and styling

--- client/components/main_pages/Home.js
+++ client/components/main_pages/Home.js
@@ -24,18 +24,61 @@ class Home extends Component {
           <div className='description'>
             <h3>MyProperties.me is a property management app for landlords in a digital world.</h3>
           </div>
-
-
         </div>
 
-        <div className='features-section'>
-          <ul>
-            <li>
-              <i className='material-icons'>check</i>
-              <h5>Awesome feature</h5>
-            </li>
-          </ul>
+        <div className='container-fluid'>
+          <div className='row features-and-coming-soon'>
+            <div className='features-section col-md-6'>
+              <h4>Features</h4>
+              <ul>
+                <li>
+                  <i className='material-icons'>check</i>
+                  <h5>Manage properties and tenants</h5>
+                </li>
+                <li>
+                  <i className='material-icons'>check</i>
+                  <h5>Track rent payments</h5>
+                </li>
+                <li>
+                  <i className='material-icons'>check</i>
+                  <h5>Get instant cash flow analysis</h5>
+                </li>
+                <li>
+                  <i className='material-icons'>check</i>
+                  <h5>Signup assistant with limited admin capabilites</h5>
+                </li>
+                <li>
+                  <i className='material-icons'>check</i>
+                  <h5>Get hourly updates on Costa Rican Colon currency conversion</h5>
+                </li>
+              </ul>
+            </div>
+
+            <div className='features-section col-md-6'>
+              <h4>Coming Soon</h4>
+              <ul>
+                <li>
+                  <i className='material-icons'>check</i>
+                  <h5>Month by month income anaylsis</h5>
+                </li>
+                <li>
+                  <i className='material-icons'>check</i>
+                  <h5>Currency convertion between all currencies</h5>
+                </li>
+                <li>
+                  <i className='material-icons'>check</i>
+                  <h5>Automated email reminders for overdue tenants</h5>
+                </li>
+                <li>
+                  <i className='material-icons'>check</i>
+                  <h5>Integrated iPhone/iPad camera for tenant contract/reciept organization</h5>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
+
+
       </div>
     )
   }
