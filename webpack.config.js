490ba7a00db539a4cd9eaad2a9188ca7ff21462e
Jimmy Gonzalez <jimmygonzalez87@gmail.com>
2019-01-08 19:47:04 -0600

    webpack test again

--- webpack.config.js
+++ webpack.config.js
@@ -26,12 +26,12 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     NODE_ENV: JSON.stringify('production')
+    //   }
+    // }),
+    // new webpack.optimize.UglifyJsPlugin()
     // new HtmlWebpackPlugin({
     //   template: 'build/index.html'
     // }),
@@ -40,12 +40,12 @@ module.exports = {
     //     NODE_ENV: JSON.stringify('production')
     //   }
     // }),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: { warnings: false },
-    //   comments: false,
-    //   sourceMap: true,
-    //   minimize: false
-    // }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      comments: false,
+      sourceMap: true,
+      minimize: true
+    }),
   ],
 
 };
